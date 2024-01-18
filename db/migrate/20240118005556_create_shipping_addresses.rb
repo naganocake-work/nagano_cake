76bfbf352b31bd2c06073cf1430c2f4bf016dff6
masaki2307 <149597889+masaki2307@users.noreply.github.com>
2024-01-18 01:22:06 +0000

    Add model

---
+++ db/migrate/20240118005556_create_shipping_addresses.rb
@@ -0,0 +1,16 @@
+class CreateShippingAddresses < ActiveRecord::Migration[6.1]
+  def change
+    create_table :shipping_addresses do |t|
+
+      t.integer :customer_id, null: false
+
+      t.string :postal_code, null: false
+
+      t.string :address, null: false
+
+      t.string :addressee_name, null: false
+
+      t.timestamps
+    end
+  end
+end
