c1476007cae17f6932b5287a1bc7b03d67828802
masaki2307 <149597889+masaki2307@users.noreply.github.com>
2024-01-21 15:09:19 +0000

    Add genre1

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,9 +1,9 @@
 class Public::CustomersController < ApplicationController
   before_action :authenticate_customer!
   def show
-    
+
     @customer = current_customer
-    
+
   end
 
   def edit
@@ -11,13 +11,13 @@ class Public::CustomersController < ApplicationController
 
   def confirm
   end
-  
+
   def disable
-    
+
   @customer = current_customer
   @customer.update(is_active: false)
   reset_session
   flash[:notice] = "退会処理を実行いたしました"
   redirect_to root_path
-  end  
+  end
 end
