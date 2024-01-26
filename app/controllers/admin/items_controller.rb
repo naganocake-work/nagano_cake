c488059865a4dc1315ceab31af28e1af42d96f30
masaki2307 <149597889+masaki2307@users.noreply.github.com>
2024-01-26 15:14:24 +0000

    add item

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -3,7 +3,14 @@ class Admin::ItemsController < ApplicationController
 
   def index
 
-    @items = Item.all
+    if params[:name].present?
+
+       @items = Item.where("name LIKE ?", "%#{params[:name]}%").page(params[:page])
+
+    else
+
+     @items = Item.page(params[:page])
+    end
 
   end
 
@@ -25,16 +32,37 @@ def create
 end
 
   def show
+
+
+    @item = Item.find(params[:id])
+
   end
 
   def edit
+
+    @item = Item.find(params[:id])
+
+  end
+
+  def update
+
+   @item = Item.find(params[:id])
+
+   if @item.update(item_params)
+    redirect_to admin_item_path(@item.id)
+   else
+    @errors = @item.errors.full_messages
+    render :edit
+   end
+
   end
 
   private
 
   def item_params
 
-    params.require(:item).permit(:name, :introduction, :price, :image, :genre_id)
+    params.require(:item).permit(:name, :introduction, :price, :image, :genre_id, :is_active)
+
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -7,11 +7,17 @@ class Item < ApplicationRecord
   validates :price, presence: true
    #validates :genre, presence: true
   belongs_to :genre
+
   def get_image(width, height)
+
   unless image.attached?
+
     file_path = Rails.root.join('app/assets/images/default-image.jpg')
     image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
   end
-  image.variant(resize_to_limit: [width, height]).processed
+
+
+    image.variant(resize_to_limit: [width, height]).processed
   end
+
 end
